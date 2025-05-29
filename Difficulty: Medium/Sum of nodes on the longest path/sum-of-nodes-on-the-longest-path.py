20bbb034c51902dfcbc6ef1d22d82bb8e05af107
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-05-29 23:40:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of nodes on the longest path/sum-of-nodes-on-the-longest-path.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.sum=0
+        self.len=0
+        
+    def dfs(self,root,curr,l):
+        curr+=root.data
+        l+=1
+        if self.len==l:
+            self.sum=max(self.sum,curr)
+        if self.len<l:
+            self.len=l
+            self.sum=curr
+        if root.left:
+            self.dfs(root.left,curr,l)
+        if root.right:
+            self.dfs(root.right,curr,l)
+    
+    def sumOfLongRootToLeafPath(self, root):
+        self.dfs(root,0,0)
+        return self.sum
