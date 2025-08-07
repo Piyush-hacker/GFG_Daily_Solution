b6df34c6b67bdfc06764412c6c255c979d30d1dc
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-08-07 13:07:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Difference Check/difference-check.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDifference(self, arr):
+        # code here
+        total = 24*60*60
+        ans= total
+        def convert(s):
+            a = list(map(int, s.split(":")))
+            return a[0]*60*60 +a[1]*60 +  a[2]
+            return curr
+        newArr = sorted(list(map(convert,arr)))
+        for i in range(1,len(newArr)):
+            ans = min(ans,newArr[i]-newArr[i-1])
+        ans = min(ans,total-newArr[-1]+newArr[0])
+        return ans
