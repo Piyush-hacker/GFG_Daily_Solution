48cf62446d01d3dc48cc9d0de5d9f4e360e4ccfb
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-10-09 13:27:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.py
@@ -0,0 +1,18 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def postOrder(self, root):
+        left = right = []
+        if root.left:
+            left = self.postOrder(root.left)
+        if root.right:
+            right = self.postOrder(root.right)
+        return [*left, *right, root.data]
+        
+        
