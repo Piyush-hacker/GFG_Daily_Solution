d27a09fe59aea53bb32307f73d4e29045184af9b
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-11-04 22:45:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Frog Jump/frog-jump.py
@@ -0,0 +1,9 @@
+class Solution:
+   def minCost(self, height):
+        n=len(height)
+        if n==1:
+            return 0
+        a,b=0,abs(height[0]-height[1])
+        for i in range(3,n+1):
+            b,a=min(b+abs(height[i-1]-height[i-2]),a+abs(height[i-1]-height[i-3])),b
+        return b
