3ccfbc3ebe02fe331e3d65a7b378d8fc3a7ed9f6
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-12-10 16:35:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def findTwoElement(self, arr):
+        d=Counter(arr)
+        res=[0,0]
+        l=len(arr)
+        for i in range(1,l+1):
+            if i in d and d[i]==2:
+                res[0]=i
+            elif i not in d:
+                res[1]=i
+        return res
