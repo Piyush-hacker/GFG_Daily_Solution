3bffd2a4cbd3129139d1fdd76c05caad7821440a
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-12-22 22:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rowWithMax1s(self, arr):
+        cMax = 0
+        iMax = -1
+        for i, row in enumerate(arr):
+            if (row.count(1)>cMax):
+                cMax = row.count(1)
+                iMax = i
+                
+        return iMax
