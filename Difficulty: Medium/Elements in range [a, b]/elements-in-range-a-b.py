33ee3fff86dde65d8091391c637d876bc237c23c
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-12-23 21:40:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Elements in range [a, b]/elements-in-range-a-b.py
@@ -0,0 +1,17 @@
+from bisect import bisect_left, bisect_right
+
+class Solution:
+
+    def cntInRange(self, arr, queries):
+
+        # code here
+
+        l=[]
+
+        arr.sort()
+
+        for a,b in queries:
+
+            l.append(bisect_right(arr,b)-bisect_left(arr,a))
+
+        return l
