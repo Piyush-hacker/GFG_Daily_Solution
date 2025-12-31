528f7ea7a553ff9243ae24f210710180261df743
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2025-12-31 18:37:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, head):
+        # code here
+        s=[]
+        while head:
+            s.append(str(head.data))
+            head = head.next
+        return all(s[i] == s[len(s) - i - 1] for i in range(len(s) // 2))
