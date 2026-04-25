0a647e141e7f03df8759fb2574e3ec7135dfc603
Piyush Nitnaware <77199821+Piyush-hacker@users.noreply.github.com>
2026-04-25 21:18:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Opposite Sign Pair Reduction/opposite-sign-pair-reduction.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reducePairs(self, arr):
+        stk=[]
+        for ve in arr:
+            cur=ve
+            while stk and (stk[-1]<0<cur or stk[-1]>0>cur):
+                cand=stk.pop()
+                if abs(cand)==abs(cur):
+                    cur=None
+                    break
+                cur=max([cur,cand],key=lambda x:abs(x))
+            if cur!=None:
+                stk.append(cur)
+        return stk
